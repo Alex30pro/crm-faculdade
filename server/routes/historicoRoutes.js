e116cc60361fca8df04a0eefc101cf2363835a7a
Alex Andryo Da Silva Araujo <alexandryopg87@gmail.com>
2026-01-02 10:21:21 -0300

    Update historicoRoutes.js

--- server/routes/historicoRoutes.js
+++ server/routes/historicoRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const router = express.Router({ mergeParams: true }); // mergeParams é importante para pegar o ID da rota pai
+const router = express.Router({ mergeParams: true }); 
 const historicoController = require('../controllers/historicoController');
 
-// A URL final será /api/contatos/:contatoId/historico
 router.get('/', historicoController.getHistoricoPorContatoId);
 
 module.exports = router;
