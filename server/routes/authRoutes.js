f0ca1935aa5ab191c0f63201b2910dddb9baa1f1
Alex Andryo Da Silva Araujo <alexandryopg87@gmail.com>
2026-01-02 10:20:33 -0300

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,17 +1,12 @@
-// ARQUIVO: server/routes/authRoutes.js (VERSÃO FINAL CORRIGIDA)
-
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { authenticateToken, isAdmin } = require('../middlewares/authMiddleware');
 
-// Rota para o login
 router.post('/login', authController.login);
 
-// Rota para mudar a senha (DEVE SER POST e precisa do authenticateToken)
 router.post('/mudar-senha', authenticateToken, authController.mudarSenha);
 
-// Rota para o cadastro
 router.post('/register', authenticateToken, isAdmin, authController.register);
 
 module.exports = router;
