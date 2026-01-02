4ef32815c5075c8b9ab08020b9ce4b3106c79591
Alex Andryo Da Silva Araujo <alexandryopg87@gmail.com>
2026-01-02 10:22:39 -0300

    Update publicRoutes.js

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -1,10 +1,7 @@
-// EM: server/routes/publicRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const publicController = require('../controllers/publicController'); // Vamos criar este arquivo a seguir
+const publicController = require('../controllers/publicController'); 
 
-// Rota para receber o cadastro (lead) dos sites externos
 router.post('/lead', publicController.createLead);
 
 module.exports = router;
