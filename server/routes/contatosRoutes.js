532d147f89bb0ae1258adf0390b5b089f0264290
Alex Andryo Da Silva Araujo <alexandryopg87@gmail.com>
2026-01-02 10:21:06 -0300

    Update contatosRoutes.js

--- server/routes/contatosRoutes.js
+++ server/routes/contatosRoutes.js
@@ -1,4 +1,3 @@
-// server/routes/contatosRoutes.js
 const express = require('express');
 const router = express.Router();
 
@@ -21,28 +20,22 @@ const {
 
 const historicoRoutes = require('./historicoRoutes');
 
-// Rotas GERAIS (sem ID)
 router.get('/', authenticateToken, getAllContatos);
 router.post('/', authenticateToken, createContato);
 router.get('/lixeira', authenticateToken, getDeletedContatos);
 
-// Rota de exportação (específica) vem ANTES da rota genérica com /:id
 router.get('/export', authenticateToken, exportarParaXLSX );
 
-// Rotas ESPECÍFICAS por ID de contato
 router.get('/:id', authenticateToken, getContatoById);
 router.put('/:id', authenticateToken, updateContato);
 router.delete('/:id', authenticateToken, deleteContato);
 router.put('/:id/restaurar', authenticateToken, restaurarContato);
 
-// Rotas de Lembrete (específicas por ID)
 router.post('/:id/lembrete', authenticateToken, agendarLembrete);
 router.delete('/:id/lembrete', authenticateToken, removerLembrete);
 
-// Rota ESPECIAL DE ADMIN por ID
 router.delete('/:id/permanent', authenticateToken, isAdmin, permanentDeleteContato);
 
-// Rotas de Histórico por ID
 router.post('/:id/historico', authenticateToken, registrarInteracaoManual);
 router.use('/:id/historico', historicoRoutes);
 
