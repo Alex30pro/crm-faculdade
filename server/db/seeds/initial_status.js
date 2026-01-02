02aa02adc844855d287f0dfdd8d1e0e62d3cb6f9
Alex Andryo Da Silva Araujo <alexandryopg87@gmail.com>
2026-01-02 10:34:43 -0300

    Update initial_status.js

--- server/db/seeds/initial_status.js
+++ server/db/seeds/initial_status.js
@@ -1,12 +1,11 @@
-// server/db/seeds/initial_status.js
 exports.seed = async function(knex) {
-  // Deleta dados existentes
+
   await knex('status').del()
-  // Insere os novos dados
+
   await knex('status').insert([
-    { id: 1, nome_status: 'Pendente', cor_hex: '#ffc107' }, // Amarelo
-    { id: 2, nome_status: 'Matriculado', cor_hex: '#28a745' }, // Verde
-    { id: 3, nome_status: 'Desistiu', cor_hex: '#dc3545' }, // Vermelho
-    { id: 4, nome_status: 'Em Negociação', cor_hex: '#007bff' } // Azul
+    { id: 1, nome_status: 'Pendente', cor_hex: '#ffc107' }, 
+    { id: 2, nome_status: 'Matriculado', cor_hex: '#28a745' }, 
+    { id: 3, nome_status: 'Desistiu', cor_hex: '#dc3545' }, 
+    { id: 4, nome_status: 'Em Negociação', cor_hex: '#007bff' } 
   ]);
 };
