8d666f828f3ae0e466a75e8a577d1c9a0b47aea6
Alex Andryo Da Silva Araujo <alexandryopg87@gmail.com>
2026-01-02 10:28:27 -0300

    Update 20250910144806_adicionar_role_e_status_aos_usuarios.js

--- server/db/migrations/20250910144806_adicionar_role_e_status_aos_usuarios.js
+++ server/db/migrations/20250910144806_adicionar_role_e_status_aos_usuarios.js
@@ -1,8 +1,8 @@
 exports.up = function(knex) {
   return knex.schema.table('usuarios', function(table) {
-    // 'admin' ou 'user'. Por padrão, toda nova conta será 'user'.
+
     table.string('role').notNullable().defaultTo('user');
-    // true se a conta estiver ativa, false se desativada.
+
     table.boolean('is_active').notNullable().defaultTo(true);
   });
 };
